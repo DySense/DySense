a60420201523290b08dc2144ebdb7985cbc495ce
Kyle McGahee <kmcgahee@ksu.edu>
2016-02-29 20:23:19 -0600

    Fixed bug in data source classes that would cause fields to be overwritten after being validated.

--- source/controller_data_sources.py
+++ source/controller_data_sources.py
@@ -4,9 +4,14 @@ class AbstractDataSource(object):
     def __init__(self, callback, controller_id='none', sensor_id='-1', sensor_metadata=None):
         
         self.callback = callback
+        self._controller_id = controller_id
+        self._sensor_id = sensor_id
+        self._receiving = False
+        self._sensor_metadata = sensor_metadata
         
-        self.update(controller_id, sensor_id, sensor_metadata, notify_callback=False)
-        
+        if self.sensor_metadata is not None:
+            self.verify_metadata()
+
     @property
     def public_info(self):
         return { 'controller_id': self.controller_id,
@@ -22,25 +27,16 @@ class AbstractDataSource(object):
     @property
     def receiving(self):
         return self._receiving
+    @receiving.setter
+    def receiving(self, new_value):
+        self._receiving = new_value
+        self.callback()
     @property
     def sensor_metadata(self):
         return self._sensor_metadata
     @property
     def sensor_output_names(self):
         return [outparam['name'] for outparam in self.sensor_metadata['data']]
-        
-    def update(self, controller_id, sensor_id, sensor_metadata, notify_callback=True):
-        
-        self._controller_id = controller_id
-        self._sensor_id = sensor_id
-        self._receiving = False
-        self._sensor_metadata = sensor_metadata
-        
-        if sensor_metadata is not None:
-            self.verify_metadata()
-
-        if notify_callback:
-            self.callback()
             
     def matches(self, sensor_id, controller_id):
         return (sensor_id == self.sensor_id) and (controller_id == self.controller_id)
@@ -51,9 +47,9 @@ class AbstractDataSource(object):
 class TimeDataSource(AbstractDataSource):
     
     def __init__(self, **kargs):
+        self.utc_time_idx = None
+        self.sys_time_idx = None
         super(TimeDataSource, self).__init__(**kargs)
-        self.utc_time_idx = -1
-        self.sys_time_idx = -1
         
     def verify_metadata(self):
         try:
@@ -65,8 +61,8 @@ class TimeDataSource(AbstractDataSource):
 class PositionDataSource(AbstractDataSource):
     
     def __init__(self, **kargs):
+        self.position_idx = None
         super(PositionDataSource, self).__init__(**kargs)
-        self.position_idx = -1
         
     def verify_metadata(self):
         try:
@@ -77,8 +73,8 @@ class PositionDataSource(AbstractDataSource):
 class OrientationDataSource(AbstractDataSource):
     
     def __init__(self, **kargs):
+        self.orientation_idx = None
         super(OrientationDataSource, self).__init__(**kargs)
-        self.orientation_idx = -1
         
     def verify_metadata(self):
         try:
